e02a058388f0d4d45a7d1aef441941448fc39f38
miujiaAAA <1396871192@qq.com>
2025-09-23 21:05:20 +0800

    代码优化

--- wbalone/src/main/java/com/wbalone/config/SecurityConfig.java
+++ wbalone/src/main/java/com/wbalone/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.wbalone.config;
 
-import com.wbalone.core.security.filter.JwtAuthenticationFilter;
+import cn.hutool.core.util.ArrayUtil;
+import com.wbalone.config.property.SecurityProperties;
+import com.wbalone.core.security.exception.MyAccessDeniedHandler;
+import com.wbalone.core.security.exception.MyAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +12,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,21 +26,39 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final String[] ignoreUrls = {"/api/v1/auth/login/**"};
-
+    private final SecurityProperties securityProperties;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(ignoreUrls).permitAll()
-                .anyRequest().authenticated()
-            );
+        return http
+                .authorizeHttpRequests(requestMatcherRegistry -> {
+                            // 忽略认证的 URI 地址
+                            String[] ignoreUrls = securityProperties.getIgnoreUrls();
+                            if (ArrayUtil.isNotEmpty(ignoreUrls)) {
+                                requestMatcherRegistry.requestMatchers(ignoreUrls).permitAll();
+                            }
+                            // 其他请求都需要认证
+                            requestMatcherRegistry.anyRequest().authenticated();
+                        }
+                )
+                .exceptionHandling(configurer ->
+                        configurer
+                                .authenticationEntryPoint(new MyAuthenticationEntryPoint()) // 未认证异常处理器
+                                .accessDeniedHandler(new MyAccessDeniedHandler()) // 无权限访问异常处理器
+                )
 
-        // 添加JWT过滤器
-
-        return http.build();
+                // 禁用默认的 Spring Security 特性，适用于前后端分离架构
+                .sessionManagement(configurer ->
+                        configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 无状态认证，不使用 Session
+                )
+                .csrf(AbstractHttpConfigurer::disable)      // 禁用 CSRF 防护，前后端分离无需此防护机制
+                .formLogin(AbstractHttpConfigurer::disable) // 禁用默认的表单登录功能，前后端分离采用 Token 认证方式
+                .httpBasic(AbstractHttpConfigurer::disable) // 禁用 HTTP Basic 认证，避免弹窗式登录
+                // 禁用 X-Frame-Options 响应头，允许页面被嵌套到 iframe 中
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                // 限流过滤器
+                // 验证码校验过滤器
+                // 验证和解析过滤器
+                .build();
     }
 
     @Bean
---
+++ wbalone/src/main/java/com/wbalone/core/security/exception/MyAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package com.wbalone.core.security.exception;
+
+import com.wbalone.result.ResultCode;
+import com.wbalone.util.ResponseUtils;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 无权限访问处理器
+ *
+ * @author Ray.Hao
+ * @since 2.0.0
+ */
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) {
+        ResponseUtils.writeErrMsg(response, ResultCode.ACCESS_UNAUTHORIZED);
+    }
+
+}
