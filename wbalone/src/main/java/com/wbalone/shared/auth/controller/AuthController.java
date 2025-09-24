e3ac2912f800ca81f3a9f0025babd39708fba51d
miujiaAAA <1396871192@qq.com>
2025-09-24 11:52:16 +0800

    代码优化

--- wbalone/src/main/java/com/wbalone/shared/auth/controller/AuthController.java
+++ wbalone/src/main/java/com/wbalone/shared/auth/controller/AuthController.java
@@ -1,23 +1,14 @@
 package com.wbalone.shared.auth.controller;
 
-import cn.hutool.core.bean.BeanUtil;
 import com.wbalone.core.security.model.AuthenticationToken;
-import com.wbalone.core.security.utils.JwtTokenProvider;
 import com.wbalone.result.Result;
 import com.wbalone.shared.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
